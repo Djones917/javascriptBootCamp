19618e512c1d4fb56a4938f361c241cfdb733192
Derek <nightwing917@hotmail.com>
2018-05-02 20:20:23 -0600

    functions

---
+++ functions/undefined-null.js
@@ -0,0 +1,36 @@
+// Undefined for variable unassigned
+
+let name;
+name = 'Jen';
+
+
+if (name === undefined) {
+   console.log('Please provide a name!');
+} else {
+    console.log(name);
+}
+
+
+// Undefined for function arguments 
+// Undefined as function return default value
+let square = function (num) {
+    console.log(num);
+}
+
+square(); // This will be undefined also undefined for below
+let result = square(); 
+console.log(result);
+
+
+
+// Null as assigned value 
+let age = 27;
+
+age = null; // Instead of assiging undefined!!!
+
+console.log(age);
+
+
+
+
+
