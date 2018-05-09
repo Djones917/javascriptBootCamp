0a357afcc703670d874df37c6d9608dc330ba070
Derek <nightwing917@hotmail.com>
2018-05-08 20:57:17 -0600

    ojects manipulation

--- objects/objects-101.js
+++ objects/objects-101.js
@@ -1,3 +1,31 @@
 let myBook = {
-    
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
 }
+
+console.log(`${myBook.title} by ${myBook.author}`); // Dot Notation with template string!
+
+
+// Reasign the value to title
+myBook.title = 'Animal Farm'; 
+
+console.log(`${myBook.title} by ${myBook.author}`); // Dot Notation with template string!
+
+
+// Challenge Area
+
+// name, age, location
+// name is age and lives in location
+// Increase age by 1 and reprint
+
+let somePerson = {
+    name: 'Vincent',
+    age: 6,
+    location: 'Utah'
+}
+
+console.log(`${somePerson.name} is ${somePerson.age} years old and lives in ${somePerson.location}`);
+
+somePerson.age = somePerson.age = 7; // add one year
+console.log(`${somePerson.name} is ${somePerson.age} years old and lives in ${somePerson.location}`);
