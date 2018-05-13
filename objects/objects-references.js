7dc26b4877107013466157214c82aa537311e65c
Derek <nightwing917@hotmail.com>
2018-05-12 19:35:33 -0600

    expense tracker

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -9,6 +9,33 @@ let addExpense = function (account, amount) {
     account.expenses = account.expenses + amount;    
 }
 
-addExpense(myAccount, 2.50); 
-console.log(myAccount);     // changing one changes the other. But they are both referencing the same object. This will print
- 
+
+// addIncome
+let addIncome = function (account, income) {
+    account.income = account.income + income;
+}
+
+// resetAccount
+let resetAccount = function (account) {
+    account.expenses = 0;
+    account.income = 0;
+}
+
+//getAccountSummary
+// Account for person 1 has $900. $1000 in income. $100 in expenses
+let getAccountSummary = function (account) {
+    let balance = account.income - account.expenses;
+    return `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses.`
+}
+
+
+ addIncome(myAccount, 2000);
+addExpense(myAccount, 2.50);
+addExpense(myAccount, 160);
+console.log(getAccountSummary(myAccount));
+
+resetAccount(myAccount);
+console.log(getAccountSummary(myAccount));
+
+
+
