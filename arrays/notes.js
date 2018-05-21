795c7b237fdd9c1f0df84219a6aefb7c69aa7585
Derek <nightwing917@hotmail.com>
2018-05-20 20:11:16 -0600

    Notes and todo is correct now

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,1 +1,11 @@
 // Switched up teaching in arrays 1 notes.js and todo.js start with array ! lesson!
+
+// MDN array for reference
+const notes = ['Note 1', 'Note 2', 'Note 3'];
+console.log(notes.length);
+console.log(notes[0]);
+
+// Another example 
+const task = ['Task 1', 'Task 2', 'Task 3'];
+//console.log(task[2]); 
+console.log(task[task.length - 2]); // another way to grab a certain array with length
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,4 +1,11 @@
-// Switched up teaching in arrays 1 notes.js and todo.js start with array ! lesson!
-// MDN array for reference
-const notes = ['Note 1', 'Note 2', 'Note 3'];
-console.log(notes.length);
+// Do a array of with five todos
+// print a message with x is actual length
+// print first and second to last items -> Todo: walk the dog
+
+const myTodos = ['Feed the cats', 'Prep the coffee', 'Brush teeth', 'Watch a little TV', 'Go to bed'];
+console.log(`You have ${myTodos.length} todos!`);
+console.log(`Todo: ${myTodos[0]}`);
+console.log(`Todo: ${myTodos[myTodos.length - 2]}`);
+
+
+
