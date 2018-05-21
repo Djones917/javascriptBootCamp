9d3df1c830f12c434263aae032f06107579cf870
Derek <nightwing917@hotmail.com>
2018-05-20 18:50:55 -0600

    array

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,1 +1,4 @@
 // Switched up teaching in arrays 1 notes.js and todo.js start with array ! lesson!
+// MDN array for reference
+const notes = ['Note 1', 'Note 2', 'Note 3'];
+console.log(notes.length);
