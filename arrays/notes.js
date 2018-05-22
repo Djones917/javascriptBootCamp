4331fc48157eef49a08f52a90076bc529d413bc2
Derek <nightwing917@hotmail.com>
2018-05-21 20:34:48 -0600

    array methods

--- arrays/notes.js
+++ arrays/notes.js
@@ -9,3 +9,11 @@ console.log(notes[0]);
 const task = ['Task 1', 'Task 2', 'Task 3'];
 //console.log(task[2]); 
 console.log(task[task.length - 2]); // another way to grab a certain array with length
+
+
+// Manipulating arrays with methods
+const name = ['Derek', 'Kim', 'Vincent'];
+console.log(name.pop()); // .pop this removes the lastt value to array
+name.push('Ivy'); // .push a value to the end of array
+console.log(name);
+
