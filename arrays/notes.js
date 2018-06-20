4b940e4b9de6c3419d91dcb27015c62ebc5d226a
Derek <nightwing917@hotmail.com>
2018-06-19 20:35:52 -0600

    sort notes added

--- arrays/notes.js
+++ arrays/notes.js
@@ -13,9 +13,17 @@ const notes = [{
 }];
 
 
+
+//Quick note of filter example:  a < b = true because 'a' comes before 'b'.  'March' < 'January' = false because J comes before M. 'a' < 'A' = capital letters come first
 const sortNotes = function () {
    notes.sort(function (a, b) {
-
+       if (a.title.toLowerCase() < b.title.toLowerCase()) {
+          return -1
+       } else if (b.title.toLowerCase() < a.title.toLowerCase()) {
+          return 1
+       } else {
+           return 0
+       }
    })
 }
 
@@ -37,19 +45,14 @@ const findNotes = function (notes, query) {
 }
 
 
-console.log(findNotes(notes, 'work'))
 
 
+//console.log(findNotes(notes, 'work'))
 
 
-//const findNote = function (notes, noteTitle) {
-    //const index = notes.findIndex(function (note, index) {
-        //return note.title.toLowerCase() === noteTitle.toLowerCase()
-    //})
-    //return notes[index]
-//}
-
-const note = findNote(notes, 'Office Modification')
-console.log(note)
+//const note = findNote(notes, 'Office Modification')
+//console.log(note)
 
 
+sortNotes(notes)
+console.log(notes)
