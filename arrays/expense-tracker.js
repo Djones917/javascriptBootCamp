7b4b5e3a5397bb1d6c5eb51971e4af92dd0a93c9
Derek <nightwing917@hotmail.com>
2018-06-23 19:24:25 -0600

    added instructions

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -2,3 +2,11 @@ const account = {
     name: 'Derek',
     expenses: []
 }
+
+// expenses -> description, ammount
+// addExpense -> description, ammount
+// getAccountSummary -> total up all expenses -> name has $1250 in expenses
+
+account.addExpense('Rent', 950)
+account.addExpense('Coffee', 2)
+console.log(getAccountSummary())
