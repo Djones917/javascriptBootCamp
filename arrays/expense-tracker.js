4d59cc71c5559d3cfff9e45052bfd3b36d572d63
Derek <nightwing917@hotmail.com>
2018-06-26 20:10:30 -0600

    modified expense tracker

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,5 +1,6 @@
 const account = {
-    name: 'Derek',
+    name: 'Derek Jones',
+    income: [],
     expenses: [],
     addExpense: function (description, amount) {
        this.expenses.push({
@@ -7,21 +8,37 @@ const account = {
            amount: amount
        })
     },
+    addIncome: function (description, amount) {
+        this.income.push({
+            description: description,
+            amount: amount
+        })
+    },
     getAccountSummary: function () {
         let totalExpenses = 0
 
         this.expenses.forEach(function(expense){
              totalExpenses = totalExpenses + expense.amount
         })
+        
+        let totalIncome = 0
 
-        return `${this.name} has $${totalExpenses} in expenses.`
+        this.income.forEach(function (income) {
+            totalIncome = totalIncome + income.amount
+        })
+
+        return `${this.name} has $${totalIncome} in income and $${totalExpenses} in expenses.`
     }
 }
 
 
+// 1. add income array to account
+// 2. add income method -> description, amount
+// 3. tweak getAccountSummary - Derek has a balance of $10. $100 in income. $90 in expenses 
 
 
 
 account.addExpense('Rent', 950)
 account.addExpense('Coffee', 2)
+account.addIncome('Job', 1000)
 console.log(account.getAccountSummary())
