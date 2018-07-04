3cc08ffe066785d5383416a0cfbde7e4bf62008c
Derek <nightwing917@hotmail.com>
2018-07-04 10:55:33 -0600

    selector all

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,18 @@
-console.log('Hello, there!')
+// Dom - Document Object Model
+
+// querySelector will match the very first selector found in html
+// query and remove
+//const p = document.querySelector('p')
+//console.log(p) 
+//p.remove()
+
+
+// query all and remove
+const ps = document.querySelectorAll('p')
+
+// textContent is a property to read the value
+ps.forEach(function (p){
+	p.textContent = '****'
+	//console.log(p.textContent) 
+	//p.remove()
+})
