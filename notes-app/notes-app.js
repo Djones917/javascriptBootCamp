ba1c8bce8c63760a742ca8883f011e5327f26231
Derek <nightwing917@hotmail.com>
2018-07-16 20:11:03 -0600

    notes-app with ids

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,7 +9,17 @@ const notes = [{
    body: 'Get a new chair'
 }];
 
-document.querySelector('button').addEventListener('click', function (e) {
+document.querySelector('#create-note').addEventListener('click', function (e) {
     e.target.textContent = 'Button was clicked!'
 })
 
+
+document.querySelector('#remove-all').addEventListener('click', function() {
+    console.log('Remove all notes')
+})
+
+// Don't delet this! Using bracket notation this will select the second button. But if you switch order of the button in html you will have also switched the function!
+// So use ids and classes in your html!
+//document.querySelectorAll('button')[1].addEventListener('click', function(){
+    //console.log('Delete all notes')
+//})
