5a109190edab323b59897d05e5faab206a203173
Derek <nightwing917@hotmail.com>
2018-11-24 19:33:03 -0700

    fixed files in todo-app

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,21 +1,16 @@
 let todos = getSavedTodos()
 
-
 const filters = {
     searchText: '',
     hideCompleted: false
 }
 
-
 renderTodos(todos, filters)
-
-
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
     renderTodos(todos, filters)
 })
-
 
 document.querySelector('#new-todo').addEventListener('submit', function (e) {
     e.preventDefault()
@@ -27,7 +22,6 @@ document.querySelector('#new-todo').addEventListener('submit', function (e) {
     renderTodos(todos, filters)
     e.target.elements.text.value = ''
 })
-
 
 document.querySelector('#hide-completed').addEventListener('change', function (e) {
     filters.hideCompleted = e.target.checked
--- todo-app/todo-functions.js
+++ todo-app/todo-functions.js
@@ -1,3 +1,4 @@
+// Fetch existing todos from localStorage
 const getSavedTodos = function () {
     const todosJSON = localStorage.getItem('todos')
 
@@ -8,17 +9,17 @@ const getSavedTodos = function () {
     }
 }
 
-
-const saveTodos = function () {
+// Save todos to localStorage
+const saveTodos = function (todos) {
     localStorage.setItem('todos', JSON.stringify(todos))
 }
 
-
+// Render application todos based on filters
 const renderTodos = function (todos, filters) {
     const filteredTodos = todos.filter(function (todo) {
         const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
         const hideCompletedMatch = !filters.hideCompleted || !todo.completed
-
+        
         return searchTextMatch && hideCompletedMatch
     })
 
@@ -26,26 +27,39 @@ const renderTodos = function (todos, filters) {
         return !todo.completed
     })
 
-    document.querySelector('#todos').innerHTML = ''    
+    document.querySelector('#todos').innerHTML = ''
     document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos))
 
-    filteredTodos.forEach(function (todo) {        
+    filteredTodos.forEach(function (todo) {
         document.querySelector('#todos').appendChild(generateTodoDOM(todo))
     })
 }
 
-
+// Get the DOM elements for an individual note
 const generateTodoDOM = function (todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    return p
+    const todoEl = document.createElement('div')
+    const checkbox = document.createElement('input')
+    const todoText = document.createElement('span')
+    const removeButton = document.createElement('button')
+
+    // Setup todo checkbox
+    checkbox.setAttribute('type', 'checkbox')
+    todoEl.appendChild(checkbox)
+
+    // Setup the todo text
+    todoText.textContent = todo.text
+    todoEl.appendChild(todoText)
+
+    // Setup the remove button
+    removeButton.textContent = 'x'
+    todoEl.appendChild(removeButton)
+
+    return todoEl
 }
 
-
+// Get the DOM elements for list summary
 const generateSummaryDOM = function (incompleteTodos) {
     const summary = document.createElement('h2')
     summary.textContent = `You have ${incompleteTodos.length} todos left`
     return summary
 }
-
-
